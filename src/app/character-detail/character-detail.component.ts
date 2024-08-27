9f81e67405600cb8f40053f59747510d33f2c8db
Prabhat Kumar <prabhatkumar@Prabhats-MacBook-Pro.local>
2024-08-27 09:12:50 +0530

    BUG Fixes

--- src/app/character-detail/character-detail.component.ts
+++ src/app/character-detail/character-detail.component.ts
@@ -1,9 +1,8 @@
-// character-detail.component.ts
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { BehaviorSubject, forkJoin } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,7 +10,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.css'],
   standalone: true, 
-  imports:[CommonModule, HttpClientModule]
+  imports: [CommonModule, HttpClientModule]
 })
 export class CharacterDetailComponent implements OnInit {
   character$ = new BehaviorSubject<any>(null);
@@ -26,7 +25,33 @@ export class CharacterDetailComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.http.get<any>(`${this.apiUrl}${id}/`).pipe(
-        tap(character => this.character$.next(character)),
+        switchMap(character => {
+          const filmRequests = character.films.map((filmUrl: string) => 
+            this.http.get(filmUrl).pipe(catchError(() => []))
+          );
+
+          const vehicleRequests = character.vehicles.map((vehicleUrl: string) =>
+            this.http.get(vehicleUrl).pipe(catchError(() => []))
+          );
+
+          const starshipRequests = character.starships.map((starshipUrl: string) =>
+            this.http.get(starshipUrl).pipe(catchError(() => []))
+          );
+
+          return forkJoin([
+            forkJoin(filmRequests),
+            forkJoin(vehicleRequests),
+            forkJoin(starshipRequests)
+          ]).pipe(
+            map(([films, vehicles, starships]) => ({
+              ...character,
+              films,
+              vehicles,
+              starships
+            }))
+          );
+        }),
+        tap(characterWithDetails => this.character$.next(characterWithDetails)),
         catchError(error => {
           console.error('Error fetching character', error);
           this.character$.next(null);
